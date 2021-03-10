fb9c7b5d0d2bbdacbc46e793db7bba9536a9d0c5
tgeaus@gamil.com <tgeaus@gamil.com>
2021-03-10 23:37:29 +0800

    "修改" "3" "4"

--- bootmain.c
+++ bootmain.c
@@ -7,8 +7,6 @@
 
 
 
-
-
 void bootmain(void)
 {
     struct elfhdr *elf;//exe文件
@@ -37,8 +35,9 @@ void readsect(void *dst, uint offset)
   outb(0x1F2, 1);   // count = 1
   outb(0x1F3, offset);
   outb(0x1F4, offset >> 8);
-  outb(0x1F5, offset >> 16);
+  outb(0x1F5, offset >> 16); 
   outb(0x1F6, (offset >> 24) | 0xE0);
+  
   outb(0x1F7, 0x20);  // cmd 0x20 - read sectors
 
   // Read data.
