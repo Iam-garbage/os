349828a999e840832cd7e858fb705c7fb78a99f4
tgeaus@gamil.com <tgeaus@gamil.com>
2021-03-11 16:03:48 +0800

    add

--- Basicknowledge/Basicknowledge.md
+++ Basicknowledge/Basicknowledge.md
@@ -1,3 +1,9 @@
 ## 长期更新
 
-## 汇编知识
+## 汇编知识 x86语法
+* in ax,dx    
+* out dx,ax   
+
+## 汇编知识 AT&T语法
+* in dx,ax    
+* out ax,dx   
--- x86.h
+++ x86.h
@@ -5,12 +5,8 @@
 //data 8位
 static inline void outb(ushort port,uchar data)
 {
-    // 这条指令实际上是 out data,port
-    //out dx,ax
-    //data 类型为 ax
-    //port 类型为 dx 
     asm volatile("out %0,%1"  
                     :  
                     : "a" (data), "d" (port));
 }
-
+ 
